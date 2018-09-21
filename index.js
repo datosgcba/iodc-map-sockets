33e19ee0778c80bfcf655dc81863098f1ee8d682
Your Name <you@example.com>
2018-09-21 11:20:15 -0300

    a

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
 });
-http.listen(var port = process.env.PORT || 3300, function(){
+var port = process.env.PORT || 3300
+http.listen(port, function(){
   console.log('listening on *:80');
 });
